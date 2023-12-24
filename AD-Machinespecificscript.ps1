842eb268ae160c23179489876d38b6a26670f16f
karamkaran <115498766+karamkaran@users.noreply.github.com>
2023-12-24 18:01:51 +0530

    Script to get size of event logs

---
+++ AD-Machinespecificscript.ps1
@@ -0,0 +1,3 @@
+﻿# 2. Checking Event Log Size
+
+Get-WinEvent -ListLog Application, Security, Setup, System -ComputerName localhost | select -Property LogName, MaximumSizeInBytes, FileSize, LastAccessTime | ft
